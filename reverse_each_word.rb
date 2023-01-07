0316cd9b2f0d8001906af46906009459480ecc04
Kate Bennert <kate.bennert@gmail.com>
2023-01-07 14:33:02 -0500

    commit tree d7aac5cca4650ab3536b031989afe581fc7bff97

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,9 @@
-# Write your code here
+# Split the string on " " to get access to each word in the sentence
+# reverse each word, add to a new array
+# Join the array of words on " " to create one string
+
+require 'pry'
+
+def reverse_each_word(sentence)
+    sentence.split.map(&:reverse).join(" ")
+end
